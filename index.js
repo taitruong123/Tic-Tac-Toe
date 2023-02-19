2cf28f42e7b822c9fa17e8b68161547fd83a2351
Tai Truong <tylertruong2902@gmail.com>
2023-02-19 12:17:23 -0700

    fix bugs

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-//  STATE
-// const gameState = {
-
-//     players: ['x', 'o'],
-//     board: [
-//       [null, null, null],
-//       [null, null, null],
-//       [null, null, null]
-//     ]
-//   }
-
-
 // STATE
-const playerText = document.getElementById(`playerText`);
-const restart = document.getElementById(`restart`);
-const cell = Array.from(document.getElementsByClassName(`cell`));
+const state = {
+  currentPlayer: "player_1",
+  playernum1: "",
+  playernum2: "",
+  
+};
+//DOM Selectors
 
-const O_TEXT = `O`
-const X_TEXT = `X`
-const currentPlayer = X_TEXT
-const spaces = Array(9).fill(null)
+const playerText = document.getElementById('playerText')
+const restartBtn = document.getElementById('restartBtn')
+const boxes = Array.from(document.getElementsByClassName('box'))
 
-const startGame = () =>{
-    cell.forEach(cell => cell.addEventListener(`click`, cellCliked))
+const winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
+
+const O_TEXT = "O"
+const X_TEXT = "X"
+let currentPlayer = X_TEXT
+let spaces = Array(9).fill(null)
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
-const cellCliked = (e) =>{
-    const id = e.target.id
+
+const boxClicked = (e) => {
+    // const id = e.target.id
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+        if(playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} has won!`
+            let winning_blocks = playerHasWon()
+
+            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
+            return
+        }
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+    }
 }
+//player victory
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+const playerHasWon = () => {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a,b,c]
+        }
+    }
+    return false;
+}
+
+ restartButtons.addEventListener('click', restart)
+
+const restart = () => {
+    spaces.fill(null)
+
+    boxes.forEach( box => {
+        box.innerText = ''
+        box.style.backgroundColor=''
+    })
+
+    playerText.innerHTML = 'Tic Tac Toe'
+
+    currentPlayer = X_TEXT
+}
+
 startGame()
